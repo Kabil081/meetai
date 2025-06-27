2ca17e23eeb71bcd394eb0106a74eadda3499091
Kabil081 <kabilram212@gmail.com>
2025-06-27 20:46:50 +0530

    main:

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home(){
   return(
-    <div className="text-4xl font-bold text-green-500">
-      <h1>Hello World!</h1>
-      <Button/>
-    </div>
+    <Button>Click me!</Button>
   );
 }
